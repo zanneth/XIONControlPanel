58529e96b9d4b84e6573ee736c74681f17a96898
Charles Magahern <charles@magahern.com>
2015-12-29 15:37:45 -0800

    Initial Commit

---
+++ XIONControlPanel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  XIONControlPanel
+//
+//  Created by Charles Magahern on 12/29/15.
+//  Copyright © 2015 XION. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
